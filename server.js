52c44f39d6cd359dfcd0e7031eeacbaf70884c52
Navneet-Singh-123 <snavneet561@gmail.com>
2023-04-25 03:58:14 +0530

    deployment

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.use(express.json({ extended: false }));
 app.use(cors({ origin: "*" }));
 app.use("/stocks", stockRoutes);
 
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("frontend/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
